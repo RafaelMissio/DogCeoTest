b8854a56d7e3161f96fc8fe359266642ac04892c
Rafael Missio <rtmissio@gmail.com>
2025-12-15 20:11:13 -0300

    Creating the core package and the Endpoints and RestClient classes.

---
+++ src/test/java/core/Endpoints.java
@@ -0,0 +1,11 @@
+package core;
+
+public class Endpoints {
+
+    public static final String LIST_ALL_BREEDS = "/breeds/list/all";
+    public static final String BREED_IMAGES = "/breed/%s/images";
+    public static final String BREED_SUBRACA_IMAGES = "/breed/%s/%s/images";
+    public static final String RANDOM_IMAGE = "/breeds/image/random";
+    public static final String RANDOM_PARANS_IMAGE = "/breeds/image/random?%s=%s";
+
+}
