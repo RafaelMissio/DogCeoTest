ec0223880adb232f31c48fe354d674f668fe5aaa
Rafael Missio <rtmissio@gmail.com>
2025-12-15 20:40:52 -0300

    Creating the models package and the BreedImagesResponse, BreedsListResponse and RandomImageResponse classes.

---
+++ src/test/java/models/BreedImagesResponse.java
@@ -0,0 +1,17 @@
+package models;
+
+import java.util.List;
+
+public class BreedImagesResponse {
+
+    private List<String> message;
+    private String status;
+
+    public List<String> getMessage() {
+        return message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
